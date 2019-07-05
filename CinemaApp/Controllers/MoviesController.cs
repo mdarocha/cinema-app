58782dfe639f720e6ffcfd7c6b51859e43f4d612
mdarocha <git@mdarocha.pl>
2019-07-05 11:57:30 +0200

    Setup models and storage for movies

--- CinemaApp/Controllers/MoviesController.cs
+++ CinemaApp/Controllers/MoviesController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CinemaApp.Models;
 
 namespace CinemaApp.Controllers
 {
     public class MoviesController : Controller
     {
+        CinemaEntities storage = new CinemaEntities();
+
         // GET: Movies
         public ActionResult Index()
         {
---
+++ CinemaApp/Models/CinemaEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CinemaApp.Models
+{
+    public class CinemaEntities : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
