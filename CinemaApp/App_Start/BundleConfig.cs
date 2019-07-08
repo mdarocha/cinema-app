766f859acb27adba8726672b7857f1cbc2aa972f
mdarocha <git@mdarocha.pl>
2019-07-08 12:13:38 +0200

    Rework movie showing list

--- CinemaApp/App_Start/BundleConfig.cs
+++ CinemaApp/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace CinemaApp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/libs").Include(
+                        "~/Scripts/modernizr-*",
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js"));
 
@@ -17,11 +18,6 @@ namespace CinemaApp
 
             bundles.Add(new ScriptBundle("~/bundles/ajax").Include(
                         "~/Scripts/jquery.unobtrusive-ajax.js"));
-
-            // Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
-            // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
