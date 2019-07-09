cdbffdf2c11b8d99be0e2d93b079fb45607e4c43
mdarocha <git@mdarocha.pl>
2019-07-09 13:13:11 +0200

    Add PlaceSelectorViewModel, modify Place model to include position

---
+++ CinemaApp/Models/ViewModels/PlaceSelectorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CinemaApp.Models
+{
+    public class PlaceSelectorViewModel
+    {
+        public Showing Showing { get; set; }
+        public List<Place> TakenPlaces { get; set; }
+
+        public int RoomWidth { get { return 18; } }
+        public int RoomHeight { get { return 12; } }
+        public int MaxTickets { get { return 10;  } }
+    }
+}
