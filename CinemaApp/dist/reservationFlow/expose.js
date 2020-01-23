69fa7bffac223ef470630d77dcda47c308385611
mdarocha <git@mdarocha.pl>
2020-01-23 18:12:02 +0100

    Fix gitignore, remove scripts

--- CinemaApp/dist/reservationFlow/expose.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var reservation_flow_1 = require("./reservation_flow");
-global.React = React;
-global.ReactDOM = ReactDOM;
-global.ReservationFlow = reservation_flow_1.ReservationFlow;
-//# sourceMappingURL=expose.js.map
--- CinemaApp/dist/reservationFlow/reservation_flow_view_model.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=reservation_flow_view_model.js.map
